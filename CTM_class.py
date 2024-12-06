031f9f4adc9dd87aff1441f72f14540e52aa9782
Amirhossein Kiani <kiani014@umn.edu>
2024-12-06 14:50:06 -0600

    Modify Mazi code and update ctm model

--- CTM_class.py
+++ CTM_class.py
@@ -1,5 +1,5 @@
 class CTMParameters:
-    def __init__(self):
+    def __init__(self, cell_length=20, vehicle_length=5, free_flow_speed=15, wave_speed=5, jam_density=0.2, segment_length=100):
         """
         Initialize the parameters for the Cell Transmission Model.
 
@@ -10,12 +10,14 @@ class CTMParameters:
             jam_density (float): Maximum density (vehicles/meter).
         """
         # Default parameter values
-        self.cell_length = 20  # meters
-        self.vehicle_length = 5  # meters
-        self.free_flow_speed = 15  # meters/second
-        self.wave_speed = 5  # w: meters/second
-        self.jam_density = 0.2  # vehicles/meter
-        self.segment_length = 100
+        self.cell_length = cell_length
+        self.vehicle_length = vehicle_length
+        self.free_flow_speed = free_flow_speed
+        self.wave_speed = wave_speed
+        self.jam_density = jam_density
+        self.segment_length = segment_length
+        
+
 
     def max_flow(self):
         """
@@ -25,19 +27,6 @@ class CTMParameters:
             float: Maximum flow (vehicles/second).
         """
         return min(self.free_flow_speed, self.wave_speed) * self.jam_density / 2
-
-    # def __repr__(self):
-    #     """
-    #     String representation of the CTM parameters.
-    #     """
-    #     return (
-    #         f"CTMParameters("
-    #         f"cell_length={self.cell_length}, "
-    #         f"free_flow_speed={self.free_flow_speed}, "
-    #         f"wave_speed={self.wave_speed}, "
-    #         f"jam_density={self.jam_density}, "
-    #         f"max_flow={self.max_flow():.2f})"
-    #     )
 
 def initialize_density(initial_density=0):
     """
@@ -60,9 +49,8 @@ def initialize_density(initial_density=0):
     return densities
 
 
-ctm_params = CTMParameters()
 ## Cell transmission model: update cell density
-def update_cell_status(densities):
+def update_cell_status(densities, ctm_params):
     num_cells = len(densities)
     new_densities = densities.copy()
     for i in range(num_cells):  # iterate over all cells
