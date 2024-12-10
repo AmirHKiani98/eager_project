5707b9fb5b303f6f3be3ddb212157fbc19c24970
Amirhossein Kiani <kiani014@umn.edu>
2024-12-09 21:00:40 -0600

    Modifying the CTMParameters

--- CTM_classic.py
+++ CTM_classic.py
@@ -18,7 +18,7 @@ ctm_params = CTMParameters() # in this example, we use the default parameters
 # " inflow is the number of vehicles enter from the previous cell to the current cell in one step"
 # " outflow is the number of vehicles exit from the current cell to the next cell in one step"
 # assumptions: jam density and max flow are constant for all cells
-def update_cell_status(time, segment_id, densities, ctm_params, entry_flow, traffic_lights_df, traffic_lights_dict_states):
+def update_cell_status(time, segment_id, densities, ctm_params, entry_flow, traffic_lights_df, traffic_lights_dict_states, ctm_params):
     num_cells = len(densities)
     new_densities = densities.copy()
     dt = ctm_params.time_step
@@ -26,20 +26,20 @@ def update_cell_status(time, segment_id, densities, ctm_params, entry_flow, traf
         if i == 0:      # first cell
             inflow = entry_flow # no inflow
         else:           # for all other cells: minimum of max flow and the flow from the previous cell
-            inflow = min(CTMParameters().max_flow(), ctm_params.free_flow_speed * densities[i-1] * dt, ctm_params.wave_speed * (ctm_params.jam_density - densities[i]) * dt)
+            inflow = min(ctm_params.max_flow(), ctm_params.free_flow_speed * densities[i-1] * dt, ctm_params.wave_speed * (ctm_params.jam_density - densities[i]) * dt)
 
         if i == num_cells - 1:  # last cell
             # check if there is a traffic light at the end of the segment
             if is_tl(segment_id, traffic_lights_df):
                 # check the status of the traffic light
                 if tl_status(time, segment_id, traffic_lights_df, traffic_lights_dict_states) == 1: # green light
-                    outflow = min(CTMParameters().max_flow(), ctm_params.free_flow_speed * densities[i] * dt, math.inf)
+                    outflow = min(ctm_params.max_flow(), ctm_params.free_flow_speed * densities[i] * dt, math.inf)
                 else:
                     outflow = 0
             else:
-                outflow = min(CTMParameters().max_flow(), ctm_params.free_flow_speed * densities[i] * dt, math.inf)
+                outflow = min(ctm_params.max_flow(), ctm_params.free_flow_speed * densities[i] * dt, math.inf)
         else:               # for all other cells: minimum of max flow and the flow to the next cell
-            outflow = min(CTMParameters().max_flow(), ctm_params.free_flow_speed * densities[i] * dt, ctm_params.wave_speed * (ctm_params.jam_density - densities[i+1]) * dt)
+            outflow = min(ctm_params.max_flow(), ctm_params.free_flow_speed * densities[i] * dt, ctm_params.wave_speed * (ctm_params.jam_density - densities[i+1]) * dt)
 
         new_densities[i] = densities[i] + (inflow - outflow) / ctm_params.cell_length   # n(t+1) = n(t) + (y(i) - y(i+1))/dx
 
