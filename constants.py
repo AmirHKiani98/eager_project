df3ffef83f2a2a754647f50b223cf49b9eef7a9a
Maziar Zamanpour <118222891+maziar0z@users.noreply.github.com>
2025-01-27 15:49:46 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 class CTMParameters:
-    def __init__(self , cell_length=6, vehicle_length=5, free_flow_speed=15, wave_speed=5, segment_length=40):
+    def __init__(self , cell_length=6, vehicle_length=5, free_flow_speed=15, wave_speed=5, segment_length=40, num_lanes=3):
         """
         Initialize the parameters for the Cell Transmission Model.
 
@@ -11,14 +11,15 @@ class CTMParameters:
         """
         # Default parameter values
         self.time_step = cell_length / free_flow_speed
+        self.num_lanes = num_lanes
         self.cell_length = cell_length
         self.vehicle_length = vehicle_length
         self.free_flow_speed = free_flow_speed
         self.wave_speed = wave_speed
-        self.jam_density = 150/1000 * cell_length
+        self.jam_density = 150/1000 * cell_length * self.num_lanes
         self.jam_density_link = 130  # jam density for the link: 130 vehicles per km
         self.segment_length = segment_length
-        self.max_flow_link = 2000 / 3600 # vehicle per seconds
+        self.max_flow_link = 2000 / 3600 * self.num_lanes # vehicle per seconds
         
         
 
@@ -29,7 +30,7 @@ class CTMParameters:
         Returns:
             float: Maximum flow (vehicles/second).
         """
-        return min(self.free_flow_speed, self.wave_speed) * self.jam_density / 2
+        return min(self.free_flow_speed, self.wave_speed) * self.jam_density * self.num_lanes / 2 
     
 
 
